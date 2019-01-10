e188c8cde381f77871ed8e23f8522437f46712c8
songwq <1260848371@qq.com>
2019-01-10 14:20:04 +0800

    鉴别诊断

--- src/main/java/com/jhmk/model/controller/JbzdzhubiaoController.java
+++ src/main/java/com/jhmk/model/controller/JbzdzhubiaoController.java
@@ -88,12 +88,15 @@ public class JbzdzhubiaoController extends BaseEntityController<BiaozhuZhubiao> 
 
     @PostMapping("/returnMainInitData")
     public void returnFirstInitData(HttpServletResponse response, @RequestBody String map) {
+        AtResponse resp = new AtResponse(System.currentTimeMillis());
         Object parse = JSONObject.parse(map);
         logger.info("接受到的数据为：{}", map);
         String s = restTemplate.postForObject(urlPropertiesConfig.getTestToolUrl() + UrlConstants.getDataByPIdAndVId, parse, String.class);
         Rule rule = JSONObject.parseObject(s, Rule.class);
         BiaozhuZhubiao biaozhuZhubiao = jbzdZhubiaoService.rule2JbzdZhubiao(rule);
-        wirte(response, biaozhuZhubiao);
+        resp.setData(biaozhuZhubiao);
+        resp.setResponseCode(ResponseCode.OK);
+        wirte(response, resp);
     }
 
 
