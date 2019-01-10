752cc2fdddd3206b906f5c6d87e03a80eb8de184
songwq <1260848371@qq.com>
2019-01-10 17:52:26 +0800

    鉴别诊断数据不存在mysql，则查询mongodb

--- src/main/java/com/jhmk/model/controller/JbzdzhubiaoController.java
+++ src/main/java/com/jhmk/model/controller/JbzdzhubiaoController.java
@@ -11,17 +11,27 @@ import com.jhmk.model.model.AtResponse;
 import com.jhmk.model.model.ResponseCode;
 import com.jhmk.model.service.JbzdZhubiaoService;
 import com.jhmk.model.util.UrlConstants;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.formula.functions.T;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -91,9 +101,52 @@ public class JbzdzhubiaoController extends BaseEntityController<BiaozhuZhubiao> 
         AtResponse resp = new AtResponse(System.currentTimeMillis());
         Object parse = JSONObject.parse(map);
         logger.info("接受到的数据为：{}", map);
-        String s = restTemplate.postForObject(urlPropertiesConfig.getTestToolUrl() + UrlConstants.getDataByPIdAndVId, parse, String.class);
-        Rule rule = JSONObject.parseObject(s, Rule.class);
-        BiaozhuZhubiao biaozhuZhubiao = jbzdZhubiaoService.rule2JbzdZhubiao(rule);
+
+        //先从msyql查询，有值直接返回，没值从mongo查询
+        JSONObject jsonObject = JSONObject.parseObject(map);
+        String pid = jsonObject.getString("pid");
+        String vid = jsonObject.getString("vid");
+        String id = jsonObject.getString("id");
+        String[] ids = null;
+        if(id!=null && id.length()>0){
+            ids=id.split("#");
+        }
+        if(ids.length==3){
+            pid = ids[1];
+            vid = ids[2];
+        }else{
+            resp.setData("参数有误");
+            resp.setResponseCode(ResponseCode.COMERROR);
+            wirte(response, resp);
+        }
+        if(pid ==null || pid=="" || vid==null || vid==""){
+            resp.setData("参数有误");
+            resp.setResponseCode(ResponseCode.COMERROR);
+            wirte(response, resp);
+        }
+        Map maps = new HashMap();
+        maps.put("patientId",pid);
+        maps.put("visitId",vid);
+
+        AtResponse atResponse = super.listDataByMap(maps, biaozhuZhubiaoRepService, "id");
+        //解析mysql查询到的数据结果
+        Map<String,Object> data = (HashMap) atResponse.getData();
+        List listData = null;
+        for (Map.Entry<String,Object> entry : data.entrySet()){
+            if(entry.getKey().equals("listData")){
+                 listData = (List)entry.getValue();
+            }
+        }
+        int num = listData.size();
+        BiaozhuZhubiao biaozhuZhubiao;
+        //mysql查询不到数据，查询mongodb
+        if(num<1){
+            String s = restTemplate.postForObject(urlPropertiesConfig.getTestToolUrl() + UrlConstants.getDataByPIdAndVId, parse, String.class);
+            Rule rule = JSONObject.parseObject(s, Rule.class);
+            biaozhuZhubiao = jbzdZhubiaoService.rule2JbzdZhubiao(rule);
+        }else{
+            biaozhuZhubiao = (BiaozhuZhubiao)listData.get(0);
+        }
         resp.setData(biaozhuZhubiao);
         resp.setResponseCode(ResponseCode.OK);
         wirte(response, resp);
